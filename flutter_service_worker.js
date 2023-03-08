148fc153e01ebf4509a071df72110350747d1103
rcaninhu <rcaninhu@gmail.com>
2023-03-08 23:42:42 +0900

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "74306bd723731f0651e10c5161e90624",
-"index.html": "63fa6640c6754ddca140167ac420d651",
-"/": "63fa6640c6754ddca140167ac420d651",
+"index.html": "63a4fb0b5ec50cdada71a101cf9d0d1e",
+"/": "63a4fb0b5ec50cdada71a101cf9d0d1e",
 "main.dart.js": "18987c8e23f123128cd75b59890499ae",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
